29702cdd67fd2d581fc2de33f74b2bfb50f7773c
FachriAzka4 <90831982+FachriAzka4@users.noreply.github.com>
2021-09-20 16:56:20 +0700

    Create Employee.java

---
+++ Employee.java
@@ -0,0 +1,21 @@
+public abstract class Employee {
+    private String name, afm;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAfm() {
+        return afm;
+    }
+
+    public void setAfm(String afm) {
+        this.afm = afm;
+    }
+    
+    public abstract int payment();
+}
